11b66d4552cfb64cf7fed693797f4f5fd5cb4c97
devilanirudh <105111739+devilanirudh@users.noreply.github.com>
2022-10-14 20:17:55 +0530

    again

--- stocks/settings.py
+++ stocks/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django-on-heroku
+#import django-on-heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,4 +128,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django-on-heroku.settings(locals())
+#django-on-heroku.settings(locals())
