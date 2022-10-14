33adf247b9db02683a10718d4a92002df02c9551
devilanirudh <105111739+devilanirudh@users.noreply.github.com>
2022-10-14 18:14:52 +0530

    final

--- stocks/settings.py
+++ stocks/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-django_heroku.settings(locals())
+import django-on-heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%qi&eyq$hlv%z@1&(p(kyv71@wkq^0h@63l9x_*#&7vri2xw!n'
+# SECRET_KEY = 'django-insecure-%qi&eyq$hlv%z@1&(p(kyv71@wkq^0h@63l9x_*#&7vri2xw!n'
+# SECRET_KEY=6aj9il2xu2vqwvnitsg@!+4-8t3%zwr@$agm7x%o%yb2t9iv' > .env
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,8 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django-on-heroku.settings(locals())
